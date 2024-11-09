130ddb05d1612bd046d870ab2fcb8ebf33f088dc
Johnny Poks <johnpokam95@gmail.com>
2024-11-09 22:14:28 +0100

    complete landing page

--- Frontend/src/components/navbar.tsx
+++ Frontend/src/components/navbar.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div style={{display: 'flex', gap: "200px"}}>
-        <Link to={'/generate'}><button>Generate QR Code</button></Link>
-        <Link to={'/scan'}><button>Scan Code</button></Link>
+    <div className="flex justify-around items-center p-4 w-full font-semibold bg-slate-200 mb-5 stick top-0 max-md:flex-col max-md:gap-5">
+      <h1 className="flex items-center max-md:basis-full">
+        Bienvenue
+        <span className="material-symbols-outlined pl-2 text-yellow-500">
+          waving_hand
+        </span>
+      </h1>
+      <Link to={"/generate"}>
+        <button className="flex items-center max-md:basis-full">
+          <span className="material-symbols-outlined pr-2">qr_code_2_add</span>
+          Generateur de QR code
+        </button>
+      </Link>
+      <Link to={"/scan"}>
+        <button className="flex items-center max-md:basis-full">
+          <span className="material-symbols-outlined pr-2">
+            qr_code_scanner
+          </span>
+          Scanner un QR code
+        </button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- Frontend/src/pages/eventpage.tsx
+++ Frontend/src/pages/eventpage.tsx
@@ -1,19 +1,133 @@
-import React from 'react'
-import Navbar from '../components/navbar'
+import Navbar from "../components/navbar";
+
+type Props = {
+  id: number;
+  name: string;
+  telephone: string;
+  status: string;
+};
+
+const Table = ({ props }: { props: Props[] }) => {
+  const getTime = () => {
+    const time = new Date();
+    const hours = time.getHours().toString().padStart(2, "0");
+    const minutes = time.getMinutes().toString().padStart(2, "0");
+    return `${hours} : ${minutes}`;
+  };
+
+  return (
+    <table className="w-full">
+      <thead>
+        <tr className="bg-gray-200">
+          <th className="px-4 py-2">Noms</th>
+          <th className="px-4 py-2">Heure d'arrivée</th>
+          <th className="px-4 py-2">Heure de sortie</th>
+          <th className="px-4 py-2">Statut</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.map((item) => (
+          <tr key={item.id}>
+            <td className="border px-4 py-2 text-center">{item.name}</td>
+            <td className="border px-4 py-2 text-center">{getTime()}</td>
+            <td className="border px-4 py-2 text-center">{getTime()}</td>
+            <td
+              className="border px-4 py-2 text-center"
+              style={{ color: item.status === "present" ? "green" : "red" }}
+            >
+              {item.status}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 const Eventpage = () => {
-  return (
-    <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around", minHeight: "100vh"}}>
-        <Navbar />
-        <h4>Welcome to the event</h4>
-            <p>You have</p>
-        <div style={{display: "flex"}}>
-            <p>50: invited</p>
-            <p>50: absent</p>
-            <p>50: present</p>
-        </div>
-    </div>
-  )
-}
+  const data = [
+    {
+      id: 1,
+      name: "John Doe",
+      telephone: "123-456-7890",
+      status: "present",
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+      telephone: "987-654-3210",
+      status: "absent",
+    },
+    {
+      id: 3,
+      name: "Bob Smith",
+      telephone: "555-555-5555",
+      status: "present",
+    },
+    {
+      id: 4,
+      name: "Alice Johnson",
+      telephone: "111-222-3333",
+      status: "absent",
+    },
+    {
+      id: 5,
+      name: "John Doe",
+      telephone: "123-456-7890",
+      status: "present",
+    },
+    {
+      id: 6,
+      name: "Jane Doe",
+      telephone: "987-654-3210",
+      status: "absent",
+    },
+    {
+      id: 7,
+      name: "Bob Smith",
+      telephone: "555-555-5555",
+      status: "present",
+    },
+    {
+      id: 8,
+      name: "Alice Johnson",
+      telephone: "111-222-3333",
+      status: "absent",
+    },
+    {
+      id: 9,
+      name: "John Doe",
+      telephone: "123-456-7890",
+      status: "present",
+    },
+    {
+      id: 10,
+      name: "Jane Doe",
+      telephone: "987-654-3210",
+      status: "absent",
+    },
+  ];
 
-export default Eventpage
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      <Navbar />
+      <div className="flex items-center justify-around w-full m-5 max-md:flex-col">
+        <p className="font-bold">Statistiques de l'événement</p>
+        <div className="flex items-center justify-around max-md:mt-5">
+          <p className="p-3 mx-3 bg-indigo-200 font-extrabold rounded-full">
+            {data.length} Invités
+          </p>
+          <p className="p-3 mx-3 bg-green-200 font-extrabold rounded-full">
+            {data.filter((item) => item.status === "present").length} Présents
+          </p>
+          <p className="p-3 mx-3 bg-red-200 font-extrabold rounded-full">
+            {data.filter((item) => item.status === "absent").length} Absents
+          </p>
+        </div>
+      </div>
+      <Table props={data} />
+    </div>
+  );
+};
+
+export default Eventpage;
